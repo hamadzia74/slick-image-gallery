003aa0585a52e60c202ed9ef2be3acfdb2a2cbd8
Hamad Zia <hamadziaa74@gmail.com>
2023-05-01 00:36:40 +0500

    Worked till Conditional Rendering of Form

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # slick-image-gallery
 A feature-rich, single page application to showcase an image gallery, with data stored in cloud. Where users can authenticate, users can also upload new images, so they can upload these to the cloud, and display in the image gallery. User can also search for and filter results. Also implement client-side routing to allow page navigation.
 
+## Project Goals
+* Implement state management: with hooks and the Context API
+* Configure client-side routing
+* Set up a serverless backend to store, manage, and synchronize data (data management and persistence) with the Firebase Cloudstore and Firebase Storage
+* Create and manage users with Google sign-in and Firebase auth.
+* Depliy our single page application to the cloud with Firebase hosting.
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import Card from './components/Card';
+import Navbar from './components/Navbar';
+import UploadForm from './components/UploadForm';
+
+const photos = [
+  'https://picsum.photos/id/1001/200/200',
+  'https://picsum.photos/id/1002/200/200',
+  'https://picsum.photos/id/1003/200/200',
+  'https://picsum.photos/id/1004/200/200',
+  'https://picsum.photos/id/1005/200/200',
+  'https://picsum.photos/id/1006/200/200',
+  'https://picsum.photos/id/1008/200/200',
+
+]
 
 function App() {
+  const [items, setItems] = useState(photos);
+  const [isCollapsed, collapse] = useState(false);
+
+  const toggle = () => collapse(!isCollapsed);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+
+      <div className="container text-center mt-5">
+        {/* <button className='btn btn-warning mx-2' onClick={() => setItems(['https://picsum.photos/id/1009/200/200', ...items])}>+ Add</button> */}
+        <button className='btn btn-success float-end' onClick={toggle}>{isCollapsed ? 'Close' : '+ Add'}</button>
+        <div className="clearfix mb-4"></div>
+        <UploadForm isVisible={isCollapsed} />
+        <h1>Gallery</h1>
+        <div className="row">
+          {/*
+          {Array.apply(null, { length: 9 }).map(() => <Card />)}
+
+         creates an array of length 9 with null values using the apply() method of the Array constructor. The null value is used as a placeholder for each element of the array.
+          Once the array is created, the map() method is called on it. The map() method creates a new array by calling the specified function on each element of the original array. In this case, the function being passed to map() is an empty function that does nothing.  */}
+          {/* {photos.map((photo) => <Card src={photo} />)} */}
+          {items.map((photo) => <Card src={photo} />)}
+        </div>
+      </div>
+    </>
   );
 }
 
