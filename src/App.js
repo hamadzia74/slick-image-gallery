fab7208e6e5fe7c70686aaf79b9a64c5b7a898ef
Hamad Zia <hamadziaa74@gmail.com>
2023-05-01 17:16:16 +0500

    Worked with form - Submit the form and apply side effects

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Card from './components/Card';
 import Navbar from './components/Navbar';
@@ -17,10 +17,31 @@ const photos = [
 ]
 
 function App() {
+  const [count, setCount] = useState();
+  // const [input, setInput] = useState();
+  const [inputs, setInputs] = useState({ title: null, file: null, path: null });
   const [items, setItems] = useState(photos);
   const [isCollapsed, collapse] = useState(false);
-
   const toggle = () => collapse(!isCollapsed);
+  // const handleOnChange = (e) => setInput(e.target.value)
+  // const handleOnChange = (e) => {
+  //   setInput({ title: e.target.value, file: e.target.files[0], path: URL.createObjectURL(e.target.files[0]) })
+  // }
+  const handleOnChange = (e) => {
+    if (e.target.name === 'file') {
+      setInputs({ ...inputs, file: e.target.files[0], path: URL.createObjectURL(e.target.files[0]) })
+    }
+    else {
+      setInputs({ ...inputs, title: e.target.value })
+    }
+  }
+  const handleOnSubmit = (e) => {
+    e.preventDefault() // the default behavior with the form is that it's going to refresh the page
+    setItems([inputs.path, ...items])
+  }
+  useEffect(() => {
+    setCount(`you have ${items.length} image${items.length > 1 ? 's' : ''}`) //use backticks because want to use some static content with local variables
+  }, [items])
   return (
     <>
       <Navbar />
@@ -29,7 +50,8 @@ function App() {
         {/* <button className='btn btn-warning mx-2' onClick={() => setItems(['https://picsum.photos/id/1009/200/200', ...items])}>+ Add</button> */}
         <button className='btn btn-success float-end' onClick={toggle}>{isCollapsed ? 'Close' : '+ Add'}</button>
         <div className="clearfix mb-4"></div>
-        <UploadForm isVisible={isCollapsed} />
+        <UploadForm isVisible={isCollapsed} onChange={handleOnChange} onSubmit={handleOnSubmit} />
+        {count}
         <h1>Gallery</h1>
         <div className="row">
           {/*
